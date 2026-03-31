2b668a05fe61aab3f28c506b4e908b212a3d994b
seymatezel <seymatezel22@gmail.com>
2026-03-31 18:58:10 +0300

    Update utils.ts

--- Features/src/lib/utils.ts
+++ Features/src/lib/utils.ts
@@ -1,16 +1,1 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export function formatDate(timestamp: number): string {
-  return new Intl.DateTimeFormat('tr-TR', {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-  }).format(new Date(timestamp));
-}
